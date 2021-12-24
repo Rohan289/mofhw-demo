cb4f7537ac9194e9b9f2f96bf530971339a2e0c8
Rohan289 <rbhowmick184@gmail.com>
2021-12-25 02:32:36 +0530

    minor fix

--- Hello.jsx
+++ Hello.jsx
@@ -2,6 +2,6 @@ import * as ReactDOM from "react-dom";
 import * as React from "react";
 
 const Hello = () => {
-    <p>Hello World!!!</p>
+    <p>Hello World</p>
 }
 ReactDOM.render(React.createElement(Hello),document.getElementById("hello"));
