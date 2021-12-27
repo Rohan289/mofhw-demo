b73f880d3856f46e470c82a47ec1a6ebd69d87f2
Rohan289 <rbhowmick184@gmail.com>
2021-12-27 09:33:32 +0530

    minor fix

--- webpack.common.js
+++ webpack.common.js
@@ -8,6 +8,14 @@ module.exports = {
     path: path.join(__dirname, "public"),
     filename: "bundle.js"
   },
+  plugins : [
+    new webpack.DefinePlugin({
+      'process.env':{  
+        'BACKEND_BASE_URL': JSON.stringify(process.env.BACKEND_BASE_URL),
+      }
+    })
+  ],
+
   module: {
     rules: [
       {
