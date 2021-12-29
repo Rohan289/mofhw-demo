f70af9f352672fff59eb35ba48b3adb4c79a81ca
Rohan289 <rbhowmick184@gmail.com>
2021-12-29 16:10:45 +0530

    Added loader

--- src/Home.jsx
+++ src/Home.jsx
@@ -3,6 +3,8 @@ import '../App.scss';
 import axios from 'axios';
 import Table from './Table.js';
 import HeatMap from './HeatMap.jsx';
+import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
+import Loader from "react-loader-spinner";
 
 class Home extends React.Component {
   constructor(props) {
@@ -28,7 +30,14 @@ class Home extends React.Component {
         headingColumns={['Name of State', 'Total Active Cases', 'New Active Cases', 'Cumulative Discharged', 'New Discharged Cases', 'Cumulative Deaths','New Death Cases']}
         title="COVID Data State Wise"
         />
-      ) : <><p>Loading!!!</p></>}
+      ) : <> <Loader
+      style={{marginLeft:"45%"}}
+      type="Puff"
+      color="#00BFFF"
+      height={100}
+      width={100}
+      timeout={3000} //3 secs
+    /></>}
       <HeatMap covidData={this.state.covidStateData} />
       
     </Fragment>
